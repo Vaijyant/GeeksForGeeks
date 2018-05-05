d890a3f401933939ddd3c539091243438b423724
Vaijyant Tomar <vaijyant.tomar@gmail.com>
2018-05-05 17:28:20 -0400

    Interview

--- experiments/src/Interview/Java.java
+++ experiments/src/Interview/Java.java
@@ -5,12 +5,17 @@ public class Java {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 50; i++) {
-            if (i % 3 == 0 && i % 5 == 0)
-                System.out.println(i + ": FizBuzz");
-            else if (i % 3 == 0)
-                System.out.println(i + ": Fiz");
-            else if (i % 5 == 0)
-                System.out.println(i + ": Buzz");
+            String ans = "";
+            if(i%3==0)
+                ans = ans+"Fiz";
+            if(i%5 == 0)
+                ans = ans+"Buzz";
+
+
+            if(ans.equals(""))
+                System.out.println(i);
+            else
+                System.out.println(ans);
         }
     }
 
