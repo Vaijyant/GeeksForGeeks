99459ddaf49cd2795013cba1f3cb5c51caa55ddb
Vaijyant Tomar <vaijyant.tomar@gmail.com>
2018-05-14 18:00:20 -0400

    New Commit

--- experiments/src/Singleton/Singleton.java
+++ experiments/src/Singleton/Singleton.java
@@ -6,11 +6,25 @@ public class Singleton {
     int n = 0;
 
     private Singleton() {
+        System.out.println("In the private Singleton()");
+    }
+
+    Singleton(String a){
+
+    }
+
+    public Singleton(int j){
+        n=j;
+        System.out.println("inside < public Singleton(int j) >: "+ n);
+
+
     }
 
     public static Singleton getInstance() {
-        if (st == null)
+        if (st == null) {
+            System.out.println("inside < public static Singleton getInstance() >");
             st = new Singleton();
+        }
         return st;
     }
 }
--- experiments/src/Singleton/SingletonMain.java
+++ experiments/src/Singleton/SingletonMain.java
@@ -2,6 +2,7 @@ package Singleton;
 
 public class SingletonMain {
     public static void main (String[] args){
+
         Singleton s1 = Singleton.getInstance();
         Singleton s2 = Singleton.getInstance();
 
@@ -10,6 +11,13 @@ public class SingletonMain {
         System.out.println("Value of s1.n: " + s1.n);
         System.out.println("Value of s2.n: " + s2.n);
 
+        Singleton sC1 = new Singleton(1);
+        System.out.println("Value of sC1.n: " + sC1.n);
 
     }
+
+    static void legal(){
+
+    }
+
 }
