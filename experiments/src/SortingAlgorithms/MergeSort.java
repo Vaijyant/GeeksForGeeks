82f7fa6d4b8cdfe3ec2334674be5245e4984bd64
Vaijyant Tomar <vaijyant.tomar@gmail.com>
2018-06-06 08:44:02 -0400

    Merge Sort

---
+++ experiments/src/SortingAlgorithms/MergeSort.java
@@ -0,0 +1,62 @@
+package SortingAlgorithms;
+
+public class MergeSort {
+
+    public static void mergeSort(int[] arr) {
+        int[] helper = new int[arr.length];
+        mergeSort(arr, helper, 0, arr.length - 1);
+    }
+
+    public static void mergeSort(int[] arr, int[] helper, int low, int high) {
+        if (low < high) {
+            int middle = (low + high) / 2;
+            mergeSort(arr, helper, low, middle);
+            mergeSort(arr, helper, middle + 1, high);
+            merge(arr, helper, low, middle, high);
+        }
+    }
+
+    public static void merge(int[] arr, int[] helper, int low, int middle, int high) {
+        for (int i = low; i <= high; i++) {
+            helper[i] = arr[i];
+        }
+
+        int helperLeft = low;
+        int helperRight = middle + 1;
+        int current = low;
+
+
+        while (helperLeft <= middle && helperRight <= high) {
+            if (helper[helperLeft] <= helper[helperRight]) {
+                arr[current] = helper[helperLeft];
+                helperLeft++;
+
+            } else {
+                arr[current] = helper[helperRight];
+                helperRight++;
+            }
+            current++;
+        }
+
+        int remaining = middle - helperLeft;
+
+        for (int i = 0; i <= remaining; i++) {
+            arr[current + i] = helper[helperLeft + i];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 8, 4, 7, 6, 6};
+
+        mergeSort(arr);
+        display(arr);
+    }
+
+    public static void display(int arr[]) {
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+}
