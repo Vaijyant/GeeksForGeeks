1b0e511c9770a93153c52892fe2fb9d58dbea326
Vaijyant Tomar <vaijyant.tomar@gmail.com>
2018-07-02 15:55:20 -0400

    CTCI: In-place replace char[] array

---
+++ experiments/src/Interview/ParsingAProgram.java
@@ -0,0 +1,49 @@
+package Interview;
+
+import java.util.Stack;
+
+public class ParsingAProgram {
+
+    public static void main(String[] args) {
+
+        String program = "{}[]()";
+
+        System.out.println(parseProgram(program));
+    }
+
+    public static boolean parseProgram(String program) {
+
+        Stack<Character> bracketStack = new Stack<>();
+
+        for (int i = 0; i < program.length(); i++) {
+
+            char ch = program.charAt(i);
+
+            if (ch == '(' || ch == '(' || ch == '[') {
+                bracketStack.push(ch);
+            } else if (ch == ')' || ch == '}' || ch == ']') {
+
+                if (bracketStack.isEmpty() || !isMatching(bracketStack.pop(), ch))
+                    return false;
+                else
+                    bracketStack.pop();
+            }
+        }
+        if (bracketStack.empty())
+            return true;
+        else
+            return false;
+
+    }
+
+    public static boolean isMatching(char char1, char char2) {
+
+        if ((char1 == '(' && char2 == ')') ||
+                (char1 == '[' && char2 == ']') ||
+                (char1 == '{' && char2 == '}')) {
+            return true;
+
+        } else
+            return false;
+    }
+}
---
+++ experiments/src/Interview/ZigZagConversion.java
@@ -0,0 +1,33 @@
+package Interview;
+
+public class ZigZagConversion {
+
+    public static void zigZag(String str, int n) {
+
+        char[] strArray = str.toCharArray();
+
+        int row = 0;
+        String st = "";
+
+        int skip = (n - 1) * 2;
+
+
+        while (row < n) {
+
+            int i = row;
+            while (i < str.length()) {
+                st = st + str.charAt(i);
+                i = i + skip - (2 * row);
+                ;
+            }
+            skip -= 2;
+            row++;
+        }
+        System.out.println(st);
+    }
+
+    public static void main(String[] args) {
+
+        zigZag("ABCDEFGHIJKL", 3);
+    }
+}
