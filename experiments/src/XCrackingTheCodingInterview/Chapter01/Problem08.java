204507d974b7b098f55b464d9f87d0e5f0c3e36b
Vaijyant Tomar <vaijyant.tomar@gmail.com>
2018-07-03 23:39:31 -0400

    CTCI: Making row and col zero if a zero is encountered in the matrix.

---
+++ experiments/src/XCrackingTheCodingInterview/Chapter01/Problem08.java
@@ -0,0 +1,59 @@
+package XCrackingTheCodingInterview.Chapter01;
+
+public class Problem08 {
+    public static int[][] method1(int[][] matrix) {
+
+        boolean row[] = new boolean[matrix.length];
+        boolean col[] = new boolean[matrix[0].length];
+
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+
+
+        // set rows zero
+        for (int i = 0; i < row.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (row[i]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        // set col zero
+        for (int i = 0; i < col.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (row[i]) {
+                    matrix[j][i] = 0;
+                }
+            }
+        }
+
+        return matrix;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {{1, 2, 3, 4, 5},
+                {6, 7, 8, 9, 0},
+                {2, 3, 4, 5, 6},
+                {7, 7, 9, 0, 2},
+                {1, 0, 3, 8, 7}};
+
+        printAnswer(method1(arr));
+    }
+
+    public static void printAnswer(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
