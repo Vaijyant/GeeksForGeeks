93345e16ae5f1d917b835a25b2ef4f0c58de796c
Vaijyant Tomar <vaijyant.tomar@gmail.com>
2018-07-03 23:25:06 -0400

    CTCI: Matrix rotation

---
+++ experiments/src/XCrackingTheCodingInterview/Chapter01/Problem07.java
@@ -0,0 +1,46 @@
+package XCrackingTheCodingInterview.Chapter01;
+
+public class Problem07 {
+
+
+    public static int[][] method1(int[][] matrix) {
+
+        int n = matrix.length;
+
+        for (int layer = 0; layer <= n / 2; layer++) {
+
+            int first = layer;
+            int last = n - 1 - layer;
+
+            for (int i = first; i < last; i++) {
+                int offset = i - first;
+                int top = matrix[first][i];
+                matrix[first][i] = matrix[last - offset][first];
+                matrix[last - offset][first] = matrix[last][last - offset];
+                matrix[last][last - offset] = matrix[i][last];
+                matrix[i][last] = top;
+            }
+        }
+
+        return matrix;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {{1, 2, 3, 4, 5},
+                {6, 7, 8, 9, 10},
+                {11, 12, 13, 14, 15},
+                {16, 17, 18, 19, 20},
+                {21, 22, 23, 24, 25}};
+
+        printAnswer(method1(arr));
+    }
+
+    public static void printAnswer(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
