c05f026ad694ad77a3aa79a0ec606ccbfc7ce40a
Vaijyant Tomar <vaijyant.tomar@gmail.com>
2018-07-08 20:43:03 -0400

    CTCI: Cats and Dogs, Queueing

---
+++ experiments/src/XCrackingTheCodingInterview/Chapter03/Problem06.java
@@ -0,0 +1,79 @@
+package XCrackingTheCodingInterview.Chapter03;
+
+
+import java.util.LinkedList;
+
+abstract class Animal {
+    private int order;
+    protected String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public boolean isOlderThan(Animal a) {
+        return this.order < a.getOrder();
+    }
+}
+
+class AnimalQueue {
+    LinkedList<Dog> dogs = new LinkedList<Dog>();
+    LinkedList<Cat> cats = new LinkedList<Cat>();
+    private int order = 0;
+
+    public void enqueue(Animal a) {
+        a.setOrder(order);
+        order++;
+
+        if (a instanceof Dog) dogs.addLast((Dog) a);
+        else cats.addLast((Cat) a);
+    }
+
+    public Animal dequeueAny() {
+        if (dogs.size() == 0) {
+            return dequeueCats();
+        } else if (cats.size() == 0) {
+            return dequeueDogs();
+        }
+
+        Dog dog = dogs.peek();
+        Cat cat = cats.peek();
+        if (dog.isOlderThan(cat))
+            return dequeueDogs();
+        else
+            return dequeueCats();
+    }
+
+    public Dog dequeueDogs() {
+        return dogs.poll();
+    }
+
+    public Cat dequeueCats() {
+
+        return cats.poll();
+    }
+}
+
+class Dog extends Animal {
+    public Dog(String name) {
+        super(name);
+    }
+}
+
+class Cat extends Animal {
+    public Cat(String name) {
+        super(name);
+    }
+}
+
+
+public class Problem06 {
+}
